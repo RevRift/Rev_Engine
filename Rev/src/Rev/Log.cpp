aee7ddabf6f2bb07dc9300a9171236b289d42c47
RevRift <89649667+RevRift@users.noreply.github.com>
2021-10-01 21:10:05 +0100

    Added premake

--- Rev/src/Rev/Log.cpp
+++ Rev/src/Rev/Log.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "Log.h"
 #include "spdlog/sinks/stdout_color_sinks.h"
 
@@ -8,13 +9,20 @@ namespace Rev {
 
 	void Log::Init()
 	{
-	// color, time stamp, name of logger, log message
-		spdlog::set_pattern("%^[%T] %n: %v%$");
+		try {
+			// start color scheme, time stamp, name of logger, log message, end color scheme
+			spdlog::set_pattern("%^[%T] %n: %v%$");
+			// got to https://github.com/gabime/spdlog/wiki/3.-Custom-formatting for more details
 
-		s_CoreLogger = spdlog::stdout_color_mt("REV");
-		s_CoreLogger->set_level(spdlog::level::trace);
+			s_CoreLogger = spdlog::stdout_color_mt("REV");
+			s_CoreLogger->set_level(spdlog::level::trace);
 
-		s_CoreLogger = spdlog::stdout_color_mt("APP");
-		s_CoreLogger->set_level(spdlog::level::trace);
+			s_CoreLogger = spdlog::stdout_color_mt("APP");
+			s_CoreLogger->set_level(spdlog::level::trace);
+		}
+		catch (const spdlog::spdlog_ex& ex)
+		{
+			std::cout << "Log Initialisation failed: %s" << ex.what() << "\n";
+		}
 	}
 }
---
+++ premake5.lua
@@ -0,0 +1,112 @@
+
+-- Engine
+
+workspace "Rev"
+    architecture "x64"
+
+    configurations
+    {
+        "Debug",
+        "Release",
+        "Dist"
+    }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Rev"
+    location "Rev"
+    kind "SharedLib"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "%{prj.name}/vendor/spdlog/include"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines
+        {
+            "REV_PLATFORM_WINDOWS",
+            "REV_BUILD_DLL"
+        }
+        
+        postbuildcommands
+        {
+            ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+        }
+    
+    filter "configurations:Debug"
+        defines "REV_DEBUG"
+        symbols "On"
+    
+    filter "configurations:Release"
+        defines "REV_RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "REV_DIST"
+        optimize "On"
+    
+
+
+-- Client
+
+project "Sandbox"
+    location "Sandbox"
+    kind "ConsoleApp"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}");
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}");
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp",
+    }
+
+    includedirs
+    {
+        "Rev/vendor/spdlog/include",
+        "Rev/src"
+    }
+
+    links
+    {
+        "Rev"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines
+        {
+            "REV_PLATFORM_WINDOWS"
+        }
+    
+    filter "configurations:Debug"
+        defines "REV_DEBUG"
+        symbols "On"
+    
+    filter "configurations:Release"
+        defines "REV_RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "REV_DIST"
+        optimize "On"
